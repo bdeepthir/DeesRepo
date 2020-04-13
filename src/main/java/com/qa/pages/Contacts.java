4ba6f2c1e3c1f5775c0f6a81555da949ddcc51c0
Deepthi <bdeepthir@gmail.com>
2020-04-13 13:10:34 -0500

    added contacts page and new method in logintest

---
+++ src/main/java/com/qa/pages/Contacts.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class Contacts {
+
+	public void contact(){
+		System.out.println("Contact page");
+	}
+}
--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,5 +1,12 @@
 package com.qa.tests;
 
 public class LoginTest {
+	
+	public void testLogin(){
+		System.out.println("Test Login  ");
+	}
 
+	public void set(){
+		System.out.println("Set method");
+	}
 }
